cf594b4db161b62e115dff222e642f26f60b2546
rickvanschalkwijk <rickvanschalkwijk@gmail.com>
2013-11-02 12:07:11 +0100

    added project model

---
+++ test/ModelsTest.java
@@ -0,0 +1,21 @@
+import org.apache.http.impl.conn.InMemoryDnsResolver;
+import org.junit.Before;
+
+import com.gargoylesoftware.htmlunit.DownloadedContent.InMemory;
+
+import play.api.test.FakeApplication;
+import play.api.test.WithApplication;
+import models.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+
+public class ModelsTest extends WithApplication{
+
+	public ModelsTest(FakeApplication app) {
+		super(app);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
